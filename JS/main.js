13e4bb136dfa02c8868d6203ea25dd909d5a8350
Salva Molina <salvadormgdaw@gmail.com>
2021-06-28 17:22:22 +0200

    section portfolio done

--- JS/main.js
+++ JS/main.js
@@ -82,3 +82,17 @@ modalCloses.forEach((modalCloses) =>{
         })
     })
 })
+
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
